3903e76d64889e7ba6dc99ca643e65f93428366b
VoVyKhang <vo.vykhang24@gmail.com>
2022-06-14 00:11:48 +0700

    add proptypes lib and fix .env

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,7 @@
 import classNames from 'classnames/bind';
 import styles from './Button.module.scss';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const cx = classNames.bind(styles);
 
@@ -63,5 +64,9 @@ function Button({
         </Comp>
     );
 }
+
+Button.propTypes = {
+    children: PropTypes.node.isRequired,
+};
 
 export default Button;
